f706a6a6ef12c98e117bd9e25ea836c56598dc6e
DY <81703520+Daisuke0713@users.noreply.github.com>
2022-04-11 14:54:20 -0500

    Delete BJ_Brand_Test directory

--- BJ_Brand_Test/MyTest1.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import ice_cream as ic # not made yet
-class TestStuff(unittest.TestCase):
-    def test1(self):
-        self.assertEquals((ic.brandSearch("bj"))[0].getName(), "Salted Caramel Core")
-if __name__ == '__main__':
-    unittest.main()
