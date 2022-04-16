9c85f56511797218f1c687391ea8e8ce72a17aa7
Jake Jasmer <jakejasmer@Jakes-MacBook-Pro.local>
2022-04-16 16:21:29 -0500

    project changes from 4/16

--- Test Code/ice_cream.py
+++ Test Code/ice_cream.py
@@ -1,32 +1,33 @@
-'''
+"""
     File@ ice_cream.py
     Author@ Daisuke Yamada, Shoko Ishikawa, Jake Jasmer, and Charlie Ney
     Course@ CS 257, Software Design, Prof Anya Vostinar
-'''
+"""
 
 import csv
 from os import path
 
+
 class IceCream:
     def __init__(self, reviews_file_name, brand_key:str, name:str, description:str, 
                 avg_rating:float, rating_count:int, ingredients:str):
-        self.brand_key = brand_key 
-        self.name = name 
+        self.brand_key = brand_key
+        self.name = name
         self.description = description
         self.avg_rating = avg_rating
         self.rating_count = rating_count
         self.ingredients = ingredients
         self.reviews = self.get_reviews(reviews_file_name)
 
-    '''
+    """
         Returns a list of reviews for this ice cream. Read the file named 
         review_file_name and iterate through it to fetch reviews for this ice
         cream. Each element of this returning list is Review object that 
         contains its attributes/fields.
-    '''
+    """
     def get_reviews(self, reviews_file_name):
-        file_path = path.relpath('../Data/' + reviews_file_name)
-        reviews_file = open(file_path, 'r')
+        file_path = path.relpath("../Data/" + reviews_file_name)
+        reviews_file = open(file_path, "r")
         reader = csv.reader(reviews_file)
         next(reader)
         reviews_list = []
@@ -35,7 +36,7 @@ class IceCream:
             if line[1] == self.brand_key:
                 # rating (if the column is empty)
                 review_rating = -1
-                if line[4] != '':
+                if line[4] != "":
                     review_rating = int(line[4])
 
                 review = Review(review_rating, line[8], line[3])
@@ -44,6 +45,13 @@ class IceCream:
         reviews_file.close()
 
         return reviews_list
+    """
+        This is the default comparison method for python so that we can compare ice creams
+    """
+    def __eq__(self, other):
+        return (self.brand_key == other.brand_key) and (self.name == other.name) \
+        and (self.description == other.description) and (self.avg_rating == other.avg_rating) \
+        and (self.rating_count == other.rating_count) and (self.ingredients == other.ingredients)
 
 class Review:
     def __init__(self, rating:int, comment:str, date:str):
@@ -51,98 +59,99 @@ class Review:
         self.comment = comment
         self.date = date
 
+
 class DataSource:
     def __init__(self, products_file_name, reviews_file_name):
-        file_path = path.relpath('../Data/' + products_file_name)
-        reviews_file = open(file_path, 'r')
+        file_path = path.relpath("../Data/" + products_file_name)
+        reviews_file = open(file_path, "r")
         reader = csv.reader(reviews_file)
         next(reader)
         
-        self.ice_cream_data_source = [[],[],[],[]] # nested list containing ice creams, sorted by barnds (as a list)
+        self.ice_cream_data_source = [[],[],[],[]] # nested list containing ice creams, sorted by brands (as a list)
     
         for line in reader:
             # brand key
             ice_cream_key = line[1]
             # average rating
             ice_cream_avg_rating = -1
-            if line[5] != '':
+            if line[5] != "":
                 ice_cream_avg_rating = float(line[5])
             # rating count
             ice_cream_ratings_count = -1
-            if line[6] != '':
+            if line[6] != "":
                 ice_cream_ratings_count = float(line[6])
             
             # make an IceCream instance and append it to the list
             ice_cream = IceCream(reviews_file_name, ice_cream_key, line[2], line[4], 
                                 ice_cream_avg_rating, ice_cream_ratings_count, line[7])
-            if ice_cream_key[-2:] == 'bj':
+            if ice_cream_key[-2:] == "bj":
                 self.ice_cream_data_source[0].append(ice_cream)
-            elif ice_cream_key[-2:] == 'hd':
+            elif ice_cream_key[-2:] == "hd":
                 self.ice_cream_data_source[1].append(ice_cream)
-            elif ice_cream_key[-2:] == 'ti':
+            elif ice_cream_key[-2:] == "ti":
                 self.ice_cream_data_source[2].append(ice_cream)
             else:
                 self.ice_cream_data_source[3].append(ice_cream)
     
     # Search by brands
-    '''
+    """
         Returns None if input is invalid. Returns the corresponding
         brand key of input if input is valid. 
-    '''
+    """
     def is_valid_brand_input(self, input) -> str:
         bj_list = ["bj", "ben and jerry's", "b&j", "ben and jerrys", "BJ", "Ben and Jerry's", "B&J", "Ben and Jerrys"]
         hd_list = ["hd", "haagen-dazs", "haagen dazs", "Haagen Dazs", "Haagen-Dazs"]
         breyers_list = ["breyers", "Breyers"]
         talenti_list = ["talenti", "Talenti"]
         if input in bj_list:
-            return 'bj'
+            return "bj"
         elif input in hd_list:
-            return 'hd'
+            return "hd"
         elif input in talenti_list:
-            return 'ti'
+            return "ti"
         elif input in breyers_list:
-            return 'br'
+            return "br"
 
         return None
 
-    '''
+    """
         Returns a list of ice creams from the brand specified by 
         input. Returns an empty list if input is not valid.
-    '''
+    """
     def search_by_brands(self, input):
         brand = self.is_valid_brand_input(input)
 
         result = []
         if brand == None:
-            print('ERROR: invalid brand name - please try again!')
-        elif brand == 'bj':
+            print("ERROR: invalid brand name - please try again!")
+        elif brand == "bj":
             result = self.ice_cream_data_source[0]
-        elif brand == 'hd':
+        elif brand == "hd":
             result = self.ice_cream_data_source[1]
-        elif brand == 'ti':
+        elif brand == "ti":
             result = self.ice_cream_data_source[2]
         else:
             result = self.ice_cream_data_source[3]
         return result
             
     # Search by ratings
-    '''
+    """
         Returns -1 if input is invalid (not a number between 0 and 5 - can be float). 
         Returns int type version of input if it is valid. 
-    '''
+    """
     def is_valid_rating_input(self, input) -> int:
         input_num = -1
         try:
             if 0 <= float(input) and float(input) <= 5:
                 input_num = float(input)
         except ValueError:
-            print('ERROR: invalid number - please try again!')
+            print("ERROR: invalid number - please try again!")
         return input_num
         
-    '''
+    """
         Returns a list of ice creams with the rating greater than or
         equal to input.
-    '''
+    """
     def search_by_ratings(self, input):
         rating = self.is_valid_rating_input(input)
         result = []
@@ -158,28 +167,28 @@ class DataSource:
         return result
     
     # Search by ingredients
-    '''
+    """
         Returns a list of ice creams with the rating greater than or
         equal to input.
-    '''
+    """
     def search_by_ingredients(self, input):
-        print('Not Yet Implemeted')
+        print("Not Yet Implemeted")
 
     # Sort by a specified field of IceCream and output top 10
     def sort_top_10(self):
-        print('Not Yet Implemeted')
+        print("Not Yet Implemeted")
 
 def main():
     datasource = DataSource("dummy_products.csv", "dummy_reviews.csv")
-    print('--------------------------------------------')
-    result1 = datasource.search_by_brands('talenti')
+    print("--------------------------------------------")
+    result1 = datasource.search_by_brands("talenti")
     for ic in result1:
-        print(ic.brand_key, '|', ic.name, '|', len(ic.reviews))
-    print('--------------------------------------------')
-    result2 = datasource.search_by_ratings('4.7')
+        print(ic.brand_key, "|", ic.name, "|", len(ic.reviews))
+    print("--------------------------------------------")
+    result2 = datasource.search_by_ratings("764rueiurewgriu")
     for ic in result2:
-        print(ic.brand_key, '|', ic.name, '|', ic.avg_rating, '|',len(ic.reviews))
-    print('--------------------------------------------')
+        print(ic.brand_key, "|", ic.name, "|", ic.avg_rating, "|",len(ic.reviews))
+    print("--------------------------------------------")
 
-if __name__=='__main__':
+if __name__=="__main__":
     main()
--- Test Code/test.py
+++ Test Code/test.py
@@ -1,4 +1,4 @@
-'''
+"""
     File@ test.py
     Author@ Daisuke Yamada, Shoko Ishikawa, Jake Jasmer, and Charlie Ney
     Course@ CS 257, Software Design, Prof Anya Vostinar
@@ -13,68 +13,135 @@
     which are subsets of actual datasets used for this project. "dummy_products.cvs"
     conatins two ice creams from the four brands, and "dummy_reviews.cvs" contains
     the corresponding reviews for these 8 ice creams.
-'''
+"""
 
 import unittest
 import ice_cream as ic
 
+
 class TestBrands(unittest.TestCase):
     def setUp(self):
+        # data_source used for the rest of tesing
         self.data_source = ic.DataSource("dummy_products.csv", "dummy_reviews.csv")
+        # eight ice creams used to compare our results to the datasets above
+        self.bj_ic1 = ic.IceCream("dummy_reviews.csv", "0_bj", "Salted Caramel Core", "description bj 1", 3.7, 208, "ingredient 1")
+        self.bj_ic2 = ic.IceCream("dummy_reviews.csv", "1_bj", "Netflix & Chilll'd™", "description bj 2", 4.0, 127, "ingredient 2")
+        self.hd_ic1 = ic.IceCream("dummy_reviews.csv", "0_hd", "White Chocolate Raspberry Truffle Ice Cream", "description hd 1", 4.9, 168, "ingredient 3")  
+        self.hd_ic2 = ic.IceCream("dummy_reviews.csv", "5_hd", "Caramel Soft Dipped Ice Cream Bar", "description hd 2", 4.9, 8, "ingredient 4")
+        self.talenti_ic1 = ic.IceCream("dummy_reviews.csv", "0_talenti", "ALPHONSO MANGO SORBETTO", "desceription ti 1", 4.7, 139, "ingredient 5")
+        self.talenti_ic2 = ic.IceCream("dummy_reviews.csv", "1_talenti", "BANANA CARAMEL CRUNCH", "description ti 2", 4.2, 25, "ingredient 6")
+        self.breyers_ic1 = ic.IceCream("dummy_reviews.csv", "0_breyers", "Natural Vanilla", "description br 1", 4.1, 467, "ingredient 7")
+        self.breyers_ic2 = ic.IceCream("dummy_reviews.csv", "1_breyers", "Homemade Vanilla", "description br 2", 4.4, 98, "ingredient 8")
 
     def tearDown(self):
         pass
 
     # test for get_reviews(review_file_name) in IceCream class
-    def test1_get_reviews(self):
-        test_ic1 = ic.IceCream('dummy_reviews.csv', '0_bj', 'Salted Caramel Core', 'random description', 3, 208, 'ingredients')
+    def test_get_reviews(self):
+        test_ic1 = ic.IceCream("dummy_reviews.csv", "0_bj", "Salted Caramel Core", "random description", 3, 208, "ingredients")
         test_ic1_reviews_list = test_ic1.reviews
         self.assertEqual(len(test_ic1_reviews_list), 2)
         self.assertIsInstance(test_ic1_reviews_list[0], ic.Review)
         self.assertEqual(test_ic1_reviews_list[0].rating, 3)
-        self.assertEqual(test_ic1_reviews_list[0].date, '2017-04-15')
+        self.assertEqual(test_ic1_reviews_list[0].date, "2017-04-15")
 
-    def test2_get_reviews_empty_columns(self):
-        test_ic2 = ic.IceCream('dummy_reviews.csv', '5_hd', 'Caramel Soft Dipped Ice Cream Bar', 'random description', 4.9, 208, 'some ingredients')
+    def test_get_reviews_empty_columns(self):
+        test_ic2 = ic.IceCream("dummy_reviews.csv", "5_hd", "Caramel Soft Dipped Ice Cream Bar", "random description", 4.9, 208, "some ingredients")
         test_ic2_reviews_list = test_ic2.reviews
         self.assertEqual(len(test_ic2_reviews_list), 2)
         self.assertIsInstance(test_ic2_reviews_list[1], ic.Review)
         self.assertEqual(test_ic2_reviews_list[1].rating, -1)
-        self.assertEqual(test_ic2_reviews_list[1].comment, '')
-        self.assertEqual(test_ic2_reviews_list[1].date, '')
+        self.assertEqual(test_ic2_reviews_list[1].comment, "")
+        self.assertEqual(test_ic2_reviews_list[1].date, "")
 
     # tests for brand search
     # test for is_valid_brand_input(string input) helper method
-    def is_valid_brand_input_success_bj(self):
+    def test_is_valid_brand_input_success_bj(self):
         bj_valid_names = ["bj", "ben and jerry's", "b&j", "ben and jerrys", "BJ", "Ben and Jerry's", "B&J", "Ben and Jerrys"]
         for name in bj_valid_names:
-            self.assertEqual(self.data_source.is_valid_brand_input(name), 'bj')
+            self.assertEqual(self.data_source.is_valid_brand_input(name), "bj")
 
     # test for is_valid_brand_input(string input) help method
-    def is_valid_brand_input_success_hd(self):
+    def test_is_valid_brand_input_success_hd(self):
         hd_valid_names = ["hd", "haagen-dazs", "haagen dazs", "Haagen Dazs", "Haagen-Dazs"]
         for name in hd_valid_names:
-            self.assertEqual(self.data_source.is_valid_brand_input(name), 'hd')
+            self.assertEqual(self.data_source.is_valid_brand_input(name), "hd")
 
     # test for is_valid_brand_input(string input) help method
-    def is_valid_brand_input_success_breyers(self):
+    def test_is_valid_brand_input_success_breyers(self):
         breyers_valid_names = ["breyers", "Breyers"]
         for name in breyers_valid_names:
-            self.assertEqual(self.data_source.is_valid_brand_input(name), 'breyers')
+            self.assertEqual(self.data_source.is_valid_brand_input(name), "br")
 
     # test for is_valid_brand_input(string input) help method
-    def is_valid_brand_input_success_talenti(self):
+    def test_is_valid_brand_input_success_talenti(self):
         talenti_valid_names = ["talenti", "Talenti"]
         for name in talenti_valid_names:
-            self.assertEqual(self.data_source.is_valid_brand_input(name), 'talenti')
+            self.assertEqual(self.data_source.is_valid_brand_input(name), "ti")
 
-    def is_invalid_brand_input(self):
+    # test for is_valid_brand_input(string input) help method
+    def test_is_invalid_brand_input(self):
         invalid_names = ["1235", " ", "", "Bj", "#$%^&*)(*&", "talen"]
         for name in invalid_names:
-            self.assertTrue(self.data_source.is_valid_brand_input(name), None)
+            self.assertEqual(self.data_source.is_valid_brand_input(name), None)
     
-    # tests for rating search
-    # test for is_valid_rating_input
+    # test for search_by_brands
+    def test_search_by_brands(self):
+        # search by Ben and Jerry's
+        bj_result = self.data_source.search_by_brands("Ben and Jerry's")
+        # bj
+        self.assertEqual(len(bj_result), 2)
+        self.assertEqual(self.bj_ic1, bj_result[0])
+        self.assertEqual(self.bj_ic2, bj_result[1])
+        # hd
+        hd_result = self.data_source.search_by_brands("Haagen Dazs")
+        self.assertEqual(len(hd_result), 2)
+        self.assertEqual(self.hd_ic1, hd_result[0])
+        self.assertEqual(self.hd_ic2, hd_result[1])
+        # ti
+        ti_result = self.data_source.search_by_brands("Talenti")
+        self.assertEqual(len(ti_result), 2)
+        self.assertEqual(self.talenti_ic1, ti_result[0])
+        self.assertEqual(self.talenti_ic2, ti_result[1])
+        # br
+        br_result = self.data_source.search_by_brands("Breyers")
+        self.assertEqual(len(br_result), 2)
+        self.assertEqual(self.breyers_ic1, ti_result[0])
+        self.assertEqual(self.breyers_ic2, ti_result[1])
+        # invlid brand
+        invalid_result = self.data_source.search_by_brands("invalid")
+        self.assertEqual(len(invalid_result), 0)
 
-if __name__ == '__main__':
+    # tests for rating sort
+    # test valid rating inputs    
+    def test_is_valid_rating(self):
+        valid_ratings = ["0", "1", "2", 3, 4, 5, 0.4, "3.99999999", 00000.1]
+        for rating in valid_ratings:
+            self.assertEqual(self.data_source.is_valid_rating_input(rating), float(rating))
+
+    # test invalid rating inputs    
+    def test_is_invalid_rating(self):
+        invalid_ratings = [-3, 100, "sdlkf"]
+        for rating in invalid_ratings:
+            self.assertEqual(self.data_source.is_valid_rating_input(rating), -1)
+
+    # test rating search output with edge case of 0 stars
+    def test_search_by_ratings_0_stars(self):
+        result = ic.search_by_ratings(0)
+        intendedResult = [self.bj_ic1, self.bj_ic2, self.hd_ic1, self.hd_ic2, self.talenti_ic1, self.talenti_ic2, self.breyers_ic1, self.breyers_ic2]
+        for x in range(len(intendedResult)):
+            self.assertEqual(intendedResult[x], result[x])
+
+    # test rating search output with 4.2 stars
+    def test_search_by_rating(self):
+        result = self.data_source.search_by_ratings(4.2)
+        expectedResult = [self.hd_ic1, self.hd_ic1, self.talenti_ic1, self.talenti_ic2, self.breyers_ic2]
+        for x in range(len(expectedResult)):
+            self.assertEqual(expectedResult[x], result[x])
+
+    # test rating search output with edge case of 5 stars
+    def test_search_by_ratings_5_stars(self):
+        self.assertEqual(self.data_source.search_by_ratings(5), [])
+
+if __name__ == "__main__":
     unittest.main()
